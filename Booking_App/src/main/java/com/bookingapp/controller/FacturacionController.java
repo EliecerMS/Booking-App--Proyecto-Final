6e70736a0126fa5ebe6c7e160fb4367f8d54b070
Kcalderon10565 <kcalderon10565@ufide.ac.cr>
2024-03-29 20:29:28 -0600

    Se creo facturacion.html y perfil.html, además se creó una base de reservaciones.html.

---
+++ Booking_App/src/main/java/com/bookingapp/controller/FacturacionController.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.bookingapp.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+
+
+/**
+ *
+ * @author kenca
+ */
+@Controller
+@Slf4j
+@RequestMapping("/facturacion")
+public class FacturacionController {
+
+@GetMapping("")
+    public String inicio (Model model) {
+        model.addAttribute("title", "Facturacion");
+        return "/facturacion/facturacion";
+    }
+
+}
+
